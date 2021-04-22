593a26d16e09fee7f588bcde985ab2e2a4155c24
apolinario-manuel <apolinariomanueljs@gmail.com>
2021-04-22 22:28:49 +0100

    Deploy Project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,155 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, TouchableOpacity, Alert , TouchableWithoutFeedback, Keyboard , FlatList, View, Text, TextInput } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+
+    const [values, setValues] = useState<number[]>([])
+    const [numberInsert, setNumberInsert] = useState<string>('')
+
+
+    const calcMedia = () => {
+        let sum = 0
+
+        for(let i = 0; i < values.length; i++)
+            sum += values[i]
+
+        Alert.alert(
+            "Resultado",
+            `Sua média é ${(sum / values.length).toFixed(3)}`,
+            [
+                { text: "OK", onPress: () => console.log("OK Pressed") }
+            ]
+        );
+    }
+
+    const handleValues = () => {
+        let arr = values
+        arr.push(parseInt(numberInsert))
+        setValues(arr)
+        setValues([...values])
+        setNumberInsert('')
+    }
+
+    const clearList = () => {
+        setValues([])
+    }
+
+    return(
+        <View style={styles.container} >
+            
+                <View style={styles.content} >
+                    
+                    <Text style={styles.age} >Idades </Text>
+                    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.form} >
+                        <TextInput 
+                            style={styles.input}
+                            keyboardType='numeric'
+                            value={numberInsert}
+                            onChangeText={(value: string) => setNumberInsert(value)}  
+                        />
+                        <TouchableOpacity 
+                            style={styles.button}
+                            onPress={handleValues}
+                            disabled={!numberInsert}
+                        >
+                            <Text style={styles.textButton} > Inserir </Text>
+                        </TouchableOpacity>
+                    </View>
+                    </TouchableWithoutFeedback>
+                    <View style={styles.list}>
+                        <FlatList
+                            data={values}
+                            renderItem={({ item, index })=> (
+                                <Text key={index} style={[
+                                    styles.textList, 
+                                    { color: (item < 20) ? '#1DA1F2': (item < 50 ) ? '#d2afff': '#ffa500'}
+                                ]} > {item} </Text>
+                            )}
+                            
+                        />
+                    </View>
+                    <TouchableOpacity style={[
+                            styles.buttonMedia, 
+                            (values.length == 0) ? { backgroundColor: '#CFCFCF'}: {backgroundColor: '#3498DB' }
+                        ]} 
+                        disabled={values.length == 0} 
+                        onPress={calcMedia} 
+                    >
+                        <Text style={styles.textButtonMedia} > Calcular Média </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttonClear} onPress={clearList} >
+                        <Text style={styles.textButtonMedia} > Limpar Lista </Text>
+                    </TouchableOpacity>
+                </View>
+            
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        paddingHorizontal: 20,
+        paddingVertical: 30,
+        backgroundColor: '#2C3E50'
+    },
+    content: {
+        flex: 1,
+        width: '100%'
+    },
+    form: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    age: {
+        color: '#fff',
+        fontSize: 20,
+        marginBottom: 20
+    },
+    input: {
+        backgroundColor: '#fff',
+        width: "45%",
+        padding: 20,
+        fontSize: 20
+    },
+    button: {
+        backgroundColor: '#1ABC9C',
+        width: "45%",
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textButton: {
+        color: '#353535',
+        fontSize: 20,
+    },
+    list:{
+        flex: 1,
+        backgroundColor: '#fff',
+        marginVertical: 10,
+        
+    },
+    textList: {
+        flex: 1,
+        width: '100%',
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 3
+    },
+    buttonMedia: {
+        padding: 15,
+        marginVertical: 5
+    },
+    buttonClear: {
+        backgroundColor: 'red',
+        padding: 15,
+        marginVertical: 5
+    },
+    textButtonMedia: {
+        color: '#fff',
+        fontSize: 20,
+        textAlign: 'center',
+    }
+})
---
+++ README.md
@@ -0,0 +1,31 @@
+
+<h1 align="center">
+  🚀 Média Aritmética
+</h1>
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [React](https://reactjs.org)
+- [React Native](https://reactnative.dev/)
+
+<p align="center">
+  <img alt="screen" src="./screen.jpeg" width="100%">
+</p>
+
+## 💻 Projeto
+
+Média Aritmética é um projecto que tem como objectivo calcular a média dado
+um conjunto de números.
+
+## Instalação 
+
+Para executar esse repositório baixe-o para sua maquina ou faça um `Git Clone`
+
+### Rodando a aplicação
+
+- abra a pasta na Shell (No windows Power Shell em modo administrador). 
+- `$ expo install` esse comando ira instalar todos os módulos necessários ao React Native
+-  `$ expo start` Ira iniciar o servidor na sua máquina 
+
+
+Feito com ♥ by Apolinário Manuel
